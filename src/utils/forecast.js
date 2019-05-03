29508e8739999c6f88b8d6d8a2002968601de13d
Daniel Albino <daniel18albino@gmail.com>
2019-05-03 15:33:03 +0100

    Add high and low  temperature to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,5 @@
 const request = require("request");
+
 const forecast = (latitude, longitude, callback) => {
   const url =
     "https://api.darksky.net/forecast/75f1b874d1a2e1b2e26479a36bc891a3/" +
@@ -12,15 +13,29 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback("Unable to find location!", undefined);
     } else {
+      console.log(body.daily.data[0]);
       const temperature = (((body.currently.temperature - 32) * 5) / 9).toFixed(
         2
       );
+      const temperatureHigh = (
+        ((body.daily.data[0].temperatureHigh - 32) * 5) /
+        9
+      ).toFixed(2);
+      const temperatureLow = (
+        ((body.daily.data[0].temperatureLow - 32) * 5) /
+        9
+      ).toFixed(2);
+
       callback(
         undefined,
         body.daily.data[0].summary +
           " it is currently: " +
           temperature +
-          "ºC out. There is a " +
+          "ºC out. This high today is " +
+          temperatureHigh +
+          " and low is " +
+          temperatureLow +
+          ". There is a " +
           body.currently.precipProbability +
           "% of rain."
       );
